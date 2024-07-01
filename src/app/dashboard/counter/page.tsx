046442ac5925802ec7a7e0a559844ee773244f22
Jamer Rafael Rodríguez Vergara <jfrodriguez@fgs.co>
2024-07-01 17:00:39 -0500

    my-dashboard

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,23 @@
+
+import { CartCounter } from "@/app/shopping-cart";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: 'Shopping Cart',
+  description: 'un simple contador de productos en el carrito de compras',
+}
+
+
+export default function CounterPage() {
+
+ 
+  return (
+    
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter value={ 20 } />
+
+    </div>
+  );
+}
---
+++ src/app/dashboard/main/page.tsx
@@ -0,0 +1,8 @@
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Hello Main page</h1>
+    </div>
+  );
+}
